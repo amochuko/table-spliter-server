0e2ba98576053f7c059b7fd8886034280269151f
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-25 13:53:57 +0100

    update: add signToken

--- src/common/utils/helpers.ts
+++ src/common/utils/helpers.ts
@@ -1,7 +1,14 @@
+import jwt from "jsonwebtoken";
+import env from "./env";
+
 export const parseIntSafe = (value: string): number | null => {
   if (/^(\d+)$/.test(value)) {
     return parseInt(value, 10);
   }
 
   return null;
+};
+
+export const signToken = (payload: string) => {
+  return jwt.sign(payload, env.JWT_SECRET, { expiresIn: "7d" });
 };
