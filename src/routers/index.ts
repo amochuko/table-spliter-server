e5d8a97648e57a1e178d570dbdbd5a1383c58bc7
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-26 13:16:36 +0100

    update: add check for home route

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -8,41 +8,41 @@ export { authRouter, homeRouter };
 export function mountRouters(app: Application) {
   const dir = __dirname;
 
-  fs.readdir(dir, (err, files) => {
-    if (err) {
-      throw err;
-    }
+  const files = fs.readdirSync(dir);
+  let routeCounter = 0;
 
-    let routeCounter = 0;
-    for (const file of files) {
-      const fPath = path.join(dir, file);
-      const stat = fs.statSync(fPath);
+  for (const file of files) {
+    const fPath = path.join(dir, file);
+    const stat = fs.statSync(fPath);
 
-      if (stat.isFile()) {
-        // console.log("File: ", fPath);
-      } else if (stat.isDirectory()) {
-        const subFiles = fs.readdirSync(fPath);
+    if (stat.isFile()) {
+      // console.log("File: ", fPath);
+    } else if (stat.isDirectory()) {
+      const subFiles = fs.readdirSync(fPath);
 
-        for (const subFile of subFiles) {
-          const subFilePath = path.join(fPath, subFile);
-          if (fs.statSync(subFilePath).isFile()) {
-            const routePath = subFile.split(".")[0];
+      for (const subFile of subFiles) {
+        const subFilePath = path.join(fPath, subFile);
+        if (fs.statSync(subFilePath).isFile()) {
+          const routePath = subFile.split(".")[0];
 
-            // eslint-disable-next-line @typescript-eslint/no-require-imports
-            const routeModule = require(subFilePath);
-            const router = routeModule.default || routeModule;
+          // eslint-disable-next-line @typescript-eslint/no-require-imports
+          const routeModule = require(subFilePath);
+          const router = routeModule.default || routeModule;
 
-            let routeName = routePath.split(/Router/)[0];
-            routeName = routeName.replace(/([A-Z])/g, "-$1").toLowerCase();
-    
+          let routeName = routePath.split(/Router/)[0];
+          routeName = routeName.replace(/([A-Z])/g, "-$1").toLowerCase();
+
+          if (routeName === "home") {
+            app.use(`/`, router);
+          } else {
             app.use(`/${routeName}`, router);
-            console.log(`${routeName} was mounted`);
-            routeCounter++;
           }
+          console.log(`Mounting router at /${routeName} from ${routePath}`);
+          routeCounter++;
         }
       }
     }
+  }
 
-    console.log(`A total of ${routeCounter} was mounted`);
-  });
+  console.log(`A total of ${routeCounter} was mounted`);
 }
