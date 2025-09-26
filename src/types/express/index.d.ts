4f21d78a7053df4bf5c44f3647033a91282f8803
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-26 13:14:37 +0100

    update: refactor type

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,9 +1,9 @@
-import { JwtPayload } from "jsonwebtoken";
+import "express";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: string | JwtPayload;
+      user?: import("../../types/auth").AuthUser;
     }
   }
 }
