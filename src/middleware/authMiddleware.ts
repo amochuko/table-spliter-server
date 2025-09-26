decd5c4b7ca3d64ab3e6979f335b516bcf2d65d5
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-26 12:38:38 +0100

    update: refactor with Auth

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import envs from "../common/utils/env";
+import { AuthUser } from "../types/auth";
 
 export const authMiddleware = async (
   req: Request,
@@ -9,19 +10,27 @@ export const authMiddleware = async (
 ) => {
   const header = req.headers.authorization;
   if (!header) {
-    return res.status(401).json({ error: "Missing auth" });
+    res.status(401).json({ error: "Missing auth" });
+    return;
   }
 
   const [, token] = header.split(" ");
 
   try {
     const payload = jwt.verify(token, envs.JWT_SECRET);
-    req.user = payload;
+
+    if (typeof payload === "string") {
+       res.status(401).json({ error: "Invalid token format!" });
+    }
+
+    console.log("authMiddleware: ", payload);
+    req.user = payload as AuthUser;
 
     next();
   } catch (err) {
     console.error("authMiddleware: ", err);
 
     res.status(401).json({ error: "Invalid token!" });
+    next(err);
   }
 };
