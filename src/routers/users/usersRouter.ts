d1e351242bc1d396fb107e5aafaa62f1e2cb1102
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-28 11:15:12 +0100

    update: add users path

--- src/routers/users/usersRouter.ts
+++ src/routers/users/usersRouter.ts
@@ -18,5 +18,28 @@ router.get("/me", authMiddleware, async (req, res) => {
 });
 
 
+router.patch("/me", async (req, res) => {
+  const { zaddr } = req.body;
+
+  if (!zaddr) {
+    res.status(400).json({ error: "Missing zaddr information" });
+    return;
+  }
+
+  const result = await sql({
+    text: `SELECT * FROM users WHERE user_id = $1
+    SET zaddr = ($2)`,
+    params: [req.user?.userId, zaddr],
+  });
+
+  const user = result.rows[0];
+  if (!user) {
+    res.status(400).json({ error: "Invalid credentials" });
+  }
+
+  res.json({
+    user: { id: user.id, username: user.email, zaddr: user.zaddr },
+  });
+});
 
 export default router;
